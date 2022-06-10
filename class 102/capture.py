5cdcb6a0345353e3d27eae91d67aa681dcc5a3d2
Everettsou <106923598+Everettsou@users.noreply.github.com>
2022-06-09 18:38:28 -0600

    Add files via upload

---
+++ class 102/capture.py
@@ -0,0 +1,12 @@
+import cv2
+def take_snapshot():
+    VideoCaptureObject = cv2.VideoCapture(0)
+    result = True
+    while(result):
+        ret,frame = VideoCaptureObject.read()
+        cv2.imwrite("image.jpg",frame)
+        result = False
+    VideoCaptureObject.release()
+    cv2.destroyAllWindows()
+
+take_snapshot()
